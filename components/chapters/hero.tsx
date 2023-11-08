52e0744673f28c43b24cbf38c867b4c1924805a1
Arthur <avprocopio@gmail.com>
2023-11-08 19:52:04 -0300

    fix hero sizes on mobile

--- components/chapters/hero.tsx
+++ components/chapters/hero.tsx
@@ -3,17 +3,17 @@ import Link from '../navigation/link';
 
 function HeroChapter() {
   return (
-    <div className="flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between h-[calc(100vh-5rem)]">
-      <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-        <p className="mt-6 mb-2 text-3xl">Olá mundo!
+    <div className="flex flex-col justify-center p-4 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between h-[calc(100vh-5rem)] md:h-[calc(100vh-2rem)]">
+      <div className="flex flex-col justify-center p-4 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+        <p className="mt-6 mb-2 text-lg sm:text-2xl md:text-4xl">Olá mundo!
         </p>
-        <h1 className="text-5xl font-bold leadi">Sou
-          <span className="text-7xl dark:text-slate-400"> Arthur Procópio</span>
+        <h1 className="text-xl sm:text-3xl md:text-4xl font-bold leadi">Sou
+          <span className="text-2xl sm:text-4xl md:text-6xl dark:text-slate-400"> Arthur Procópio</span>
         </h1>
-        <p className="mt-6 text-3xl">
+        <p className="mt-6 text-sm sm:text-lg md:text-xl lg:text-2xl">
           Engenheiro de Software
         </p>
-        <p className="mb-6 text-3xl">
+        <p className="mb-6 text-sm sm:text-lg md:text-xl lg:text-2xl">
           Desenvolvedor Full Stack
         </p>
         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
@@ -27,7 +27,7 @@ function HeroChapter() {
           </Link>
         </div>
       </div>
-      <div className="relative flex items-center justify-center flex-grow p-6 mt-8 lg:mt-0">
+      <div className="relative flex items-center justify-center flex-grow p-2 lg:p-6 mt-1 md:mt-8 lg:mt-0">
         <Image
           alt="Hero"
           src="/images/hero.png"
